40e1e2086a5d9711b9d63aefa2b68fa70883874b
Manuel Smith <riddicksmith37@gmail.com>
2024-01-27 18:05:53 +0300

    Added implementation for rate limiting

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import {createWriteStream} from 'node:fs'
 import bodyParser from 'body-parser';
+import rateLimit from 'express-rate-limit';
 const output = createWriteStream('output.ndjson');
 
 
+const limiter = rateLimit({
+    windowMs: 1000, // every second
+    max: 3, // Limit each Ip to 100 requests per `Window` (here, per second)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 
 const app = express();
 app.use(bodyParser.json());
+app.use(limiter);
 
 const PORT = 3000;
 
